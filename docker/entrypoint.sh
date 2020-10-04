684c6ef4ff6c1746adcdaaf9f1dea0c55f36ae19
Alexey Kharlamov <alexey@kharlamov.biz>
2020-10-04 03:40:39 +0100

    HRegionServer info port parameterization

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,6 +4,7 @@ set -x
 ROOTDIR=/opt/hannibal
 
 sed -e "s/%HBASE_QUORUM%/${HANNIBAL_HBASE_QUORUM}/" \
+    -e "s/%HBASE_INFO_PORT%/${HANNIBAL_HBASE_INFO_PORT}/" \
     $ROOTDIR/conf/hbase-site.template.xml \
     >$ROOTDIR/conf/hbase-site.xml
 
